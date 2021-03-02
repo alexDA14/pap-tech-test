4ead95938ddb6b8f4a3b236be020b53bc15251a5
Alexey Strelnikov <alexey.strelnikov@da-14.com>
2021-03-02 22:23:33 +0200

    Clean master branch, like just created project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    CoreModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
